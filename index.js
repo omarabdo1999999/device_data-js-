e38ef11297aa9a1dd8e200d7a3f06c0f3b538369
omarrady1899 <mostafa.nassar@outlook.com>
2024-01-08 16:37:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-app.post("/saveData", async (req, res) => {
+app.post("https://azure-outfit.cyclic.app/saveData", async (req, res) => {
   try {
     const data = req.body;
 
