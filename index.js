1182deb4c7117e85497b92e0234654be0db6cbca
omarrady1899 <mostafa.nassar@outlook.com>
2024-01-08 16:03:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 // const fs = require('fs').promises;  // Import the fs module
 const path = require("path");
 const AWS = require("aws-sdk");
